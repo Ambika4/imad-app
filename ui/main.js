5a189f2121d48cc41756024e5b689cd0625a9a97
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2017-08-23 09:35:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var element = document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft + 10;
-    img.style.marginLeft=marginLeft + 'px';
+    madi.style.marginLeft=marginLeft + 'px';
 }
 madi.onclick =function()
 {
