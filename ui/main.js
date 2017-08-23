a487a1b84d0dfd2d7bed949c39475b23bfab0a44
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2017-08-23 12:39:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,9 @@
 console.log('Loaded!');
 //change the text
-var element = document.getElementById('main-text');
-element.innerHTML='New value';
-//now move  the element
-
-var element = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 10;
-    madi.style.marginLeft=marginLeft + 'px';
-}
-madi.onclick =function()
-{
-    var interval = setInterval(moveRight,100);
+var counter =0;
+var button = document.getElementById('counter');
+button.onclick =function(){
+    counter=counter+ 1; 
+    var span document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
