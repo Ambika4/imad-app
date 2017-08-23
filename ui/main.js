45b4687a4551b3e630ebba5dd6820e91460ed42e
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2017-08-23 09:24:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,12 @@ element.innerHTML='New value';
 //now move  the element
 
 var element = document.getElementById('madi');
-
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 madi.onclick =function()
 {
-    madi.style.marginLeft ="100px";
+    var Interval = setInterval(moveRight,100);
 }
