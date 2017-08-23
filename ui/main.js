86cc5b9d614b7a01f18866c7633b7a185f6f7217
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2017-08-23 09:16:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,11 @@ console.log('Loaded!');
 //change the text
 var element = document.getElementById('main-text');
 element.innerHTML='New value';
+//now move  the element
+
+var element = document.getElementById('madi');
+
+madi.onclick =function()
+{
+    madi.style.marginLeft ="100px";
+}
