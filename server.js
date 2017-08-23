3b99145bc9f02abe7a8fe09d817bc4cc37882434
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2017-08-23 08:36:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/article-three', function (req, res) {
   res.send('Article three requested and will be servered there');
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
