811192b83ad740191882c54ea23505639a81d2fb
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2017-08-29 00:59:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,16 @@ var button = document.getElementById('counter');
 var counter=0;
 button.onclick =function(){
     
-    
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if (request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+               varcounter= request.responseText;
+            }
+        }
+        
+    };
+    request.open('GET',"https://kambika1309.imad.hasura-app.io/counter",true);
+    request.send(null);
 };
     
