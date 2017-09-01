2f4ef3087f59506b04a386c18752294fdff98952
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2017-09-02 00:29:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,9 +105,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name',function (req,res){
+app.get('/submit-name/',function (req,res){
     //get the name for the request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     //JSON
