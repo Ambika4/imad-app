17934f471ae27eef9a8544d2294a153f14dee8a3
Ambika Kumari <30942672+Ambika4@users.noreply.github.com>
2017-09-02 00:11:26 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,27 @@
-
-//change the text
-
+//counter code
 var button = document.getElementById('counter');
+button.onclick = function() {
 
-button.onclick =function(){
-    
-    var request = new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if (request.readyState === XMLHttpRequest.DONE){
-            if(request.status===200){
-               var counter= request.responseText;
-               var span=document.getElementById('count');
-               span.innerHTML=counter.toString();
-            }
-        }
-        
-    };
-    request.open('GET',"http://kambika1309.imad.hasura-app.io/counter",true);
-    request.send(null);
+//Create A Request To Counter Endpoint
+var request = new XMLHttpRequest();
+//Capture The Response And Store In HTML
+request.onreadystatechange = function() {
+if (request.readyState === XMLHttpRequest.DONE) 
+{
+//Take Some Action
+if (request.status === 200) 
+ { 
+var counter= request.responseText; 
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+   }
+  }
 };
-  //submit name
+//Make A Request
+request.open('GET', 'http://shubhamshekhar089.imad.hasura-app.io/counter', true);
+request.send(null);
+};
+//submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
@@ -35,4 +37,20 @@ submit.onclick=function(){
   var ul = document.getElementById('namelist');
   ul.innerHTML = list;
 };
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
